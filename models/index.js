2b706046397e3fb6621a1aa64bbb7c1776c56ed5
unknown <annieyashley@gmail.com>
2023-05-10 12:06:21 -0500

    configuring many-to-many relations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const User = require('./User')
 const Book = require('./Book');
 const Genre = require('./Genre')
 const BookGenre = require('./BookGenre')
@@ -6,3 +7,23 @@ const BookGenre = require('./BookGenre')
 Book.belongsTo(Genre, {
     foreignKey: "genre_id"
 })
+
+Book.hasOne(Genre, {
+    foreignKey: "genre_id"
+})
+
+Genre.hasMany(Book, {
+    foreignKey: "genre_id",
+    onDelete: 'CASCADE'
+})
+
+Genre.belongsToMany(Book, {
+    through: BookGenre
+})
+
+module.exports = {
+    User,
+    Book,
+    Genre,
+    BookGenre,
+  };
